256884f09ebc9aa624216e6991838eefafa806db
Daniel <danikoynov2006@gmail.com>
2025-07-20 19:47:05 +0300

    feat: expanded order dependence functionality and added docstrings

--- orders.py
+++ orders.py
@@ -1,10 +1,32 @@
 class Order:
-    def __init__(self, side, order_type, quantity, 
-                 price = None, stop_price = None, execution_index = None):
+    """
+    Represents an order
+
+    Attributes
+    ---------
+    side : string
+        The side of the order (either buy or sell).
+    order_type : string
+        The type of the order (market, limit or stop).
+    quantity : float
+        The amount of shares requested in the order.
+    price : float
+        The price at which a limit order is executed
+    stop_price: float
+        The price at which a stop order is executed
+    order_index: int
+        The unique index of the order
+    blocking_index: int
+        A list with indexes of orders, whose execution, cancels or removes the current order
+    
+    """
+    def __init__(self, side: str, order_type: str, quantity: float, 
+                 price: float = None, stop_price: float = None, order_index: int = None, blocking_index: list[int] = None):
         self.side = side
         self.order_type = order_type
         self.quantity = quantity
         self.price = price
         self.stop_price = stop_price
-        self.execution_index = execution_index
+        self.order_index = order_index
+        self.blocking_index = blocking_index
     
