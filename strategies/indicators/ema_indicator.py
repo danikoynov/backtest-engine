71ea00f4876da40a0404b70098b94572fb81709a
Daniel <danikoynov2006@gmail.com>
2025-10-07 13:00:29 +0200

    refactor: EMA Indicator works with values instead of Candles

--- strategies/indicators/ema_indicator.py
+++ strategies/indicators/ema_indicator.py
@@ -6,22 +6,18 @@ class ExponentialMovingAverage:
 
     Attributes
     ----------
-    candlesticks : list of Candle
-        A list containing the history of candlesticks.
     periods : list of integers
         A list containing the periods to be tracked.
     ema_history : dict
         Contains pairs with tracked period and ema history (period -> list of ema-s)
     """
     def __init__(self, periods : list[int]):
-
-        self.candlesticks = list()
         self.periods = periods
         self.ema_history = {period : [] for period in self.periods}
 
-    def update(self, candlestick : Candle):
+    def update(self, value : float):
         """
-        Updates the ema-s for all tracked periods.
+        Updates the EMA-s for all tracked periods.
 
         Uses standard formula for the alpha:
             (alpha = 2 / (N + 1))
@@ -36,9 +32,8 @@ class ExponentialMovingAverage:
         -------
         None
         """
-        self.candlesticks.append(candlestick)
         for period in self.periods:
             alpha =  2.0 / (period + 1)
             last_ema = self.ema_history[period][-1] if self.ema_history[period] else 0.0
-            new_ema = last_ema * (1 - alpha) + candlestick.close_price * alpha
+            new_ema = last_ema * (1 - alpha) + value * alpha
             self.ema_history[period].append(new_ema)
